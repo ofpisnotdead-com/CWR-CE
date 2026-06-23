ef2954379ea3ca45767c10ccbf8a8d61af8503af
lowercase-t <lowercase-t@protonmail.com>
2026-06-23 12:44:03 +0200

    migrate ShowTarget/ShowGroupDir to macro

--- engine/Poseidon/Network/NetworkClientActions.cpp
+++ engine/Poseidon/Network/NetworkClientActions.cpp
@@ -2277,8 +2277,8 @@ void NetworkClient::ShowTarget(Person* vehicle, TargetType* target)
     }
 
     ShowTargetMessage msg;
-    msg.vehicle = vehicle;
-    msg.target = target;
+    msg._vehicle = vehicle;
+    msg._target = target;
     SendMsg(&msg, NMFGuaranteed);
 }
 
@@ -2290,8 +2290,8 @@ void NetworkClient::ShowGroupDir(Person* vehicle, Vector3Par dir)
     }
 
     ShowGroupDirMessage msg;
-    msg.vehicle = vehicle;
-    msg.dir = dir;
+    msg._vehicle = vehicle;
+    msg._dir = dir;
     SendMsg(&msg, NMFGuaranteed);
 }
 
