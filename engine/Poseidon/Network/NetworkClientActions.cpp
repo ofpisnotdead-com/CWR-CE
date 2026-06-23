ad2818061403e8dfb663c98d614ee6a2e1948d62
lowercase-t <lowercase-t@protonmail.com>
2026-06-23 11:08:42 +0200

    migrate AskForChangePosition to macro

--- engine/Poseidon/Network/NetworkClientActions.cpp
+++ engine/Poseidon/Network/NetworkClientActions.cpp
@@ -1635,9 +1635,9 @@ void NetworkClient::AskForChangePosition(Person* soldier, Transport* vehicle, UI
     }
 
     AskForChangePositionMessage msg;
-    msg.soldier = soldier;
-    msg.vehicle = vehicle;
-    msg.type = type;
+    msg._soldier = soldier;
+    msg._vehicle = vehicle;
+    msg._type = type;
     SendMsg(&msg, NMFGuaranteed);
 }
 
