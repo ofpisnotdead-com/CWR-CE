4b8792d3771886e3110c349cf6fc841c8dfdb557
Björn Dahlgren <bjorn@dahlgren.io>
2026-07-01 20:59:45 +0200

    Linux arm64

--- engine/Poseidon/Foundation/Math/V3QuadsP3.cpp
+++ engine/Poseidon/Foundation/Math/V3QuadsP3.cpp
@@ -7,10 +7,14 @@
 #include <Poseidon/Foundation/Math/V3Quads.hpp>
 #include <Poseidon/Graphics/Core/TLVertex.hpp>
 #include <Poseidon/Foundation/Math/Math3D.hpp>
+#ifdef __aarch64__
+#include <sse2neon/sse2neon.h>
+#else
 #ifdef _MSC_VER
 #include <intrin.h> // For MMX intrinsics
 #else
 #include <x86intrin.h>
+#endif
 #endif
 
 #if defined __ICL
